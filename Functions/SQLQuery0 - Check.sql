31fe06a1b5df0d3ccd21de27748c122926b379c8
un <un@DESKTOP-JAU235M>
2026-03-09 18:03:21 +0300

    Holidays DONE

--- Functions/SQLQuery0 - Check.sql
+++ Functions/SQLQuery0 - Check.sql
@@ -2,6 +2,17 @@
 USE PV_521_Import
 SET DATEFIRST 1;
 
-EXEC sp_SelectScheduleFor N'PV_521 ';
-PRINT dbo.GetNextLearningDay(N'PV_521', DEFAULT);
-PRINT dbo.GetNextLearningDate(N'PV_521', N'2026-03-06');
+--EXEC sp_SelectScheduleFor N'PV_521 ';
+--PRINT dbo.GetNextLearningDay(N'PV_521', DEFAULT);
+--PRINT dbo.GetNextLearningDate(N'SPU_411', N'2026-03-7');
+--PRINT dbo.GetSummerTimeSadness(2025);
+--PRINT dbo.GetEasterDate(2026);
+
+EXEC sp_InsertAllHolidaysFor 2028;
+
+SELECT
+		 [Дата]		=	[date]
+		,[Праздник]	=	holiday_name
+FROM DaysOFF, Holidays
+WHERE holiday = holiday_id
+AND		[date] >= DATEFROMPARTS(2027, 12, 20);
