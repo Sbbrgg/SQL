15828bb5b5703117a9fd9444d3e45e64a6eb68c7
un <un@DESKTOP-JAU235M>
2026-03-13 21:39:42 +0300

    SCHEDULE1221 DONE

--- StoredProcedurs/SQLQuery2 - sp_InsertSchedule1221.sql
+++ StoredProcedurs/SQLQuery2 - sp_InsertSchedule1221.sql
@@ -19,13 +19,14 @@ GO
 --END
 
 CREATE OR ALTER PROCEDURE sp_InsertSchedule1221
-		 @group_name		AS	NCHAR(10)
-		,@discipline_name	AS	NVARCHAR(150)
-		,@teacher_name		AS	NVARCHAR(50)
-		,@start_date		AS	DATE
-		,@constant_day		AS	TINYINT
-		,@alternate_day		AS	TINYINT
-		,@denied_day		AS	TINYINT
+		 @group_name			AS	NCHAR(10)
+		,@discipline_name		AS	NVARCHAR(150)
+		,@teacher_name			AS	NVARCHAR(50)
+		,@start_date			AS	DATE
+		,@constant_day			AS	TINYINT
+		,@alternate_day			AS	TINYINT
+		--,@denied_day			AS	TINYINT
+		,@first_week_present	AS	BIT
 AS
 BEGIN
 	DECLARE @group				AS	INT			=	(SELECT group_id			FROM Groups			WHERE group_name		= @group_name);
@@ -39,19 +40,20 @@ BEGIN
 	DECLARE @date				AS	DATE		=	@start_date;
 	DECLARE @time				AS	TIME		=	@start_time;
 	
+	DECLARE @previous_week_present	AS	BIT		= IIF(@first_week_present = 1, 0, 1);
+
 	WHILE @lesson_number < @number_of_lessons
 	BEGIN
-			SET		@time = @start_time;
-			EXEC sp_InsertLesson @group, @discipline, @teacher, @date, @time OUTPUT, @lesson_number OUTPUT;
-			EXEC sp_InsertLesson @group, @discipline, @teacher, @date, @time OUTPUT, @lesson_number OUTPUT;
-			--IF		DATEPART(WEEKDAY, @date) = @constant_day	SET @date = DATEADD(DAY, 7, @date);
-			SET		@date = dbo.GetNextLearningDate(@group_name, @date);
-			WHILE	DATEPART(WEEKDAY, @date) = @denied_day	
+			SET		@time = @start_time;		
+			IF DATEPART(WEEKDAY, @date) = @constant_day OR (DATEPART(WEEKDAY, @date) = @alternate_day AND @previous_week_present = 0)
 			BEGIN
-				SET	@date = dbo.GetNextLearningDate(@group_name, @date);
-				IF	DATEPART(WEEKDAY, @date) = @alternate_day 
-				AND EXISTS (SELECT lesson_id FROM Schedule WHERE [date] = DATEADD(DAY, -7, @date) AND discipline = @discipline)
-					SET @date = dbo.GetNextLearningDate(@group_name, @date);
+				EXEC sp_InsertLesson @group, @discipline, @teacher, @date, @time OUTPUT, @lesson_number OUTPUT;
+				EXEC sp_InsertLesson @group, @discipline, @teacher, @date, @time OUTPUT, @lesson_number OUTPUT;
 			END
+			
+			IF		DATEPART(WEEKDAY, @date) = @alternate_day
+				SET @previous_week_present = IIF(@previous_week_present = 1, 0, 1);
+			
+			SET @date = dbo.GetNextLearningDate(@group_name, @date);
 	END
 END
